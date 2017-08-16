b71f2dfe113a1fc017e5ff5942cf56862ea9fc98
Vedarth <vedarth.sharma@gmail.com>
2017-08-16 11:47:36 +0530

    fixed time issue

--- remove-friends.py
+++ remove-friends.py
@@ -15,14 +15,14 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 for friend in tweepy.Cursor(api.friends).items(1000):
-	#following.append((friend.followers_count,friend.screen_name))
-        try:
-            friend.unfollow()
-            print('unfollowed',friend.screen_name)
-            sleep(75)
-        except tweepy.TweepError as e:
-            print(e)
-            sleep(15*60)
+    #following.append((friend.followers_count,friend.screen_name))
+    try:
+        friend.unfollow()
+        print('unfollowed',friend.screen_name)
+        sleep(80)
+    except tweepy.TweepError as e:
+        print(e)
+        sleep(60*60)
 
 #following.sort(reverse = True)
 #print (following)
