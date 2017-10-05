476a0b4bfb06dae893bc35643628e3486477edaf
Vedarth <vedarth.sharma@gmail.com>
2017-10-05 23:48:46 +0530

    Integrate psycopg2 5

--- streaming_bot.py
+++ streaming_bot.py
@@ -14,6 +14,7 @@ except ModuleNotFoundError:
     consumer_key = os.environ['consumer_key']
     access_token = os.environ['access_token']
     access_token_secret = os.environ['access_token_secret']
+    DATABASE_URL = os.environ['database_url']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
